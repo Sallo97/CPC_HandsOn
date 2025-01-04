9391ad2b4993c0eb1dacdb280e12acdb4570847c
Sal <salerno.salvatore1997@gmail.com>
2025-01-04 11:08:48 +0100

    added function to construct prefix matrix

--- HandsOn/handson3_dynamic_programming/src/lib.rs
+++ HandsOn/handson3_dynamic_programming/src/lib.rs
@@ -17,9 +17,7 @@
 ///
 // ----------------------------------------------------------------
 
-/// Represents a Matrix, storing all the data in a
-/// single Vector. Offers methods to work with the vector
-/// using it as a 2D Matrix.
+/// Represents a fixed size Matrix.
 struct CustomMatrix {
     rows: usize,
     cols: usize,
@@ -49,8 +47,8 @@ impl CustomMatrix {
     }
 
     /// Returns the value at row `i` and col `j`
-    pub fn get_value(&self, i: &usize, j: &usize) -> Option<&u8> {
-        self.data.get(self.get_index(i, j))
+    pub fn get_value(&self, i: &usize, j: &usize) -> &u8 {
+        self.data.get(self.get_index(i, j)).unwrap_or(&0u8)
     }
 
     /// Set the value at row `i` and col `j` equal to
@@ -62,15 +60,49 @@ impl CustomMatrix {
     }
 }
 
-/// This function solves PROBLEM #1.
-/// Given:
-/// - `D` = number of days
-/// - `C` = a matrix of size n x D describing the activities
-///         available in each city. More formally:
-///         ∀i ∈ [1, n].∀j ∈ [1, D].C[i][j] = the number of
-///         activities available on the i-th city on the j-th
-///         day in a row.
-/// Returns the maximum number of activities of a optimal planning
-/// over the cities C on D days.
-/// In case an error occurs returns `None`
-pub fn get_maximum_activities() {}
+/// Given in input the `mtx` matrix
+/// it returns the matrix `sum` s.t.
+/// ∀i ∈ [0, n-1].∀j ∈ [0, m-1].sum[i][j] = Σ_{k=0 -> j}mtx[i][k]
+fn construct_prefix_sum(mtx: CustomMatrix) -> CustomMatrix {
+    let mut sum_prefix = CustomMatrix::new(mtx.rows, mtx.cols);
+
+    for i in 0..mtx.rows {
+        for j in 0..mtx.cols {
+            if j == 0 {
+                sum_prefix.set_value(&i, &j, mtx.get_value(&i, &j).clone());
+            } else {
+                let val = sum_prefix.get_value(&i, &(j - 1)) + mtx.get_value(&i, &j);
+                sum_prefix.set_value(&i, &j, val);
+            }
+        }
+    }
+    sum_prefix
+}
+
+#[cfg(test)]
+mod prefix_sum_test {
+    use crate::{construct_prefix_sum, CustomMatrix};
+
+    #[test]
+    fn psum_1() {
+        let mut mtx = CustomMatrix::new(2, 3);
+        // Construct mtx:
+        // 3 2 1
+        // 3 1 1
+        mtx.set_value(&0, &0, 3); // 3 2 1
+        mtx.set_value(&0, &1, 2);
+        mtx.set_value(&0, &2, 1);
+
+        mtx.set_value(&1, &0, 3); // 3 1 1
+        mtx.set_value(&1, &1, 1);
+        mtx.set_value(&1, &2, 1);
+
+        let sum = construct_prefix_sum(mtx);
+        assert_eq!(sum.get_value(&0, &0), &3);
+        assert_eq!(sum.get_value(&0, &1), &5);
+        assert_eq!(sum.get_value(&0, &2), &6);
+        assert_eq!(sum.get_value(&1, &0), &3);
+        assert_eq!(sum.get_value(&1, &1), &4);
+        assert_eq!(sum.get_value(&1, &2), &5);
+    }
+}
